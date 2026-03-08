8e4ef41e26769523a6c7d190a5085734c87d1cd6
Freddie Ridell <freddie@codeenplace.dev>
2026-03-08 23:32:57 +0000

    Record mutation test results and remove failing test

--- examples/vitest-js/src/wednesday.test.js
+++
@@ -1,6 +0,0 @@
-import { test, expect } from "vitest";
-import { childsDay } from "./index.js";
-
-test("wednesday's child is full of woe", () => {
-  expect(childsDay(new Date("2026-02-25"))).toBe("WRONG VALUE");
-});
