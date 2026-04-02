525109325e2230a680f36f2eb51d56ef1848cf94
Freddie Ridell <freddie@codeenplace.dev>
2026-04-02 18:43:17 +0100

    Parallelize TwigWalker using worker threads and channels

--- crates/bough-glob/src/walker.rs
+++ crates/bough-glob/src/walker.rs
@@ -1,12 +1,19 @@
 use bough_fs::{Root, Twig};
 use crate::Glob;
-use std::collections::VecDeque;
+use crossbeam_channel::Receiver;
 use std::path::PathBuf;
+use std::sync::Arc;
 
 pub struct TwigWalker<'a, R: Root> {
     root: &'a R,
     includes: Vec<Glob>,
     excludes: Vec<Glob>,
+}
+
+enum WorkerMsg {
+    Dir(PathBuf),
+    File(Twig),
+    Done,
 }
 
 impl<'a, R: Root> TwigWalker<'a, R> {
@@ -30,54 +37,115 @@ impl<'a, R: Root> TwigWalker<'a, R> {
 
     pub fn iter(self) -> TwigWalkerIter {
         let root_path = self.root.path().to_path_buf();
+        let includes = Arc::new(self.includes);
+        let excludes = Arc::new(self.excludes);
+
+        let num_workers = std::thread::available_parallelism()
+            .map(|n| n.get())
+            .unwrap_or(4);
+
+        let (work_txs, work_rxs): (Vec<_>, Vec<_>) = (0..num_workers)
+            .map(|_| crossbeam_channel::unbounded::<Option<PathBuf>>())
+            .unzip();
+
+        let (msg_tx, msg_rx) = crossbeam_channel::unbounded::<WorkerMsg>();
+
+        let mut worker_handles = Vec::with_capacity(num_workers);
+        for work_rx in work_rxs {
+            let root_path = root_path.clone();
+            let includes = Arc::clone(&includes);
+            let excludes = Arc::clone(&excludes);
+            let msg_tx = msg_tx.clone();
+
+            let handle = std::thread::spawn(move || {
+                while let Ok(Some(dir)) = work_rx.recv() {
+                    let Ok(entries) = std::fs::read_dir(&dir) else {
+                        let _ = msg_tx.send(WorkerMsg::Done);
+                        continue;
+                    };
+
+                    for entry in entries {
+                        let Ok(entry) = entry else { continue };
+                        let Ok(ft) = entry.file_type() else { continue };
+                        let path = entry.path();
+
+                        if ft.is_dir() || ft.is_symlink() {
+                            let _ = msg_tx.send(WorkerMsg::Dir(path));
+                        } else if ft.is_file() {
+                            let Some(rel) = path.strip_prefix(&root_path).ok() else {
+                                continue;
+                            };
+                            let inc = includes.iter().any(|g| g.is_match(rel));
+                            let exc = excludes.iter().any(|g| g.is_match(rel));
+                            if inc && !exc {
+                                if let Ok(twig) = Twig::new(rel.to_path_buf()) {
+                                    let _ = msg_tx.send(WorkerMsg::File(twig));
+                                }
+                            }
+                        }
+                    }
+                    let _ = msg_tx.send(WorkerMsg::Done);
+                }
+            });
+            worker_handles.push(handle);
+        }
+        drop(msg_tx);
+
+        let (result_tx, result_rx) = crossbeam_channel::unbounded::<Twig>();
+
+        let manager_handle = std::thread::spawn(move || {
+            let mut pending: Vec<PathBuf> = vec![root_path];
+            let mut in_flight: usize = 0;
+            let mut next_worker: usize = 0;
+
+            loop {
+                while !pending.is_empty() && in_flight < num_workers {
+                    let dir = pending.pop().unwrap();
+                    let _ = work_txs[next_worker].send(Some(dir));
+                    next_worker = (next_worker + 1) % num_workers;
+                    in_flight += 1;
+                }
+
+                if in_flight == 0 {
+                    for tx in &work_txs {
+                        let _ = tx.send(None);
+                    }
+                    return;
+                }
+
+                let Ok(msg) = msg_rx.recv() else { return };
+                match msg {
+                    WorkerMsg::Dir(dir) => {
+                        pending.push(dir);
+                    }
+                    WorkerMsg::File(twig) => {
+                        let _ = result_tx.send(twig);
+                    }
+                    WorkerMsg::Done => {
+                        in_flight -= 1;
+                    }
+                }
+            }
+        });
+
         TwigWalkerIter {
-            root_path: root_path.clone(),
-            includes: self.includes,
-            excludes: self.excludes,
-            queue: VecDeque::from([root_path]),
+            result_rx,
+            _manager: manager_handle,
+            _workers: worker_handles,
         }
     }
 }
 
 pub struct TwigWalkerIter {
-    root_path: PathBuf,
-    includes: Vec<Glob>,
-    excludes: Vec<Glob>,
-    queue: VecDeque<PathBuf>,
+    result_rx: Receiver<Twig>,
+    _manager: std::thread::JoinHandle<()>,
+    _workers: Vec<std::thread::JoinHandle<()>>,
 }
 
 impl Iterator for TwigWalkerIter {
     type Item = Twig;
 
     fn next(&mut self) -> Option<Self::Item> {
-        loop {
-            let path = self.queue.pop_front()?;
-
-            if path.is_symlink() || path.is_dir() {
-                let Ok(entries) = std::fs::read_dir(&path) else {
-                    continue;
-                };
-                let mut children: Vec<PathBuf> = entries
-                    .filter_map(|e| e.ok().map(|e| e.path()))
-                    .collect();
-                children.sort();
-                for child in children.into_iter().rev() {
-                    self.queue.push_front(child);
-                }
-                continue;
-            }
-
-            if !path.is_file() {
-                continue;
-            }
-
-            let rel = path.strip_prefix(&self.root_path).ok()?;
-            let matched_include = self.includes.iter().any(|g| g.is_match(rel));
-            let matched_exclude = self.excludes.iter().any(|g| g.is_match(rel));
-
-            if matched_include && !matched_exclude {
-                return Twig::new(rel.to_path_buf()).ok();
-            }
-        }
+        self.result_rx.recv().ok()
     }
 }
